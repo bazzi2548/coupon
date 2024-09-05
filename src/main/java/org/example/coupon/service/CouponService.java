e964a296ea34522151c3a67a3323eacb562f5871
문정환 <70272622+bazzi2548@users.noreply.github.com>
2024-09-06 00:39:35 +0900

    refactor: 각 Service의 역할과 책임 분리

--- src/main/java/org/example/coupon/service/CouponService.java
+++ src/main/java/org/example/coupon/service/CouponService.java
@@ -1,14 +1,11 @@
-package org.example.coupon.service.coupon;
+package org.example.coupon.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.coupon.domain.coupon.CouponIssuance;
 import org.example.coupon.dto.request.IssuanceCouponRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Service
@@ -16,13 +13,14 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 @Slf4j
 public class CouponService {
 
-    private final IssuanceService issuanceService;
+    private final CacheService cacheService;
+    private final BatchService batchService;
     private final ConcurrentLinkedQueue<IssuanceCouponRequest> queue = new ConcurrentLinkedQueue<>();
 
     public String requestCouponIssuance(IssuanceCouponRequest request) {
 
-        Long currentInventory = issuanceService.getCouponInventory(request.couponId());
-        if (issuanceService.isDuplicateRequest(request.userId(), request.couponId())) {
+        Long currentInventory = cacheService.getCouponInventory(request.couponId());
+        if (cacheService.isDuplicateRequest(request.userId(), request.couponId())) {
             return "Duplicate request detected. Coupon already issued.";
         }
 
@@ -37,20 +35,20 @@ public class CouponService {
     @Scheduled(fixedRate = 1000)
     public void processQueue() {
         log.info("스케쥴러 실행");
-        List<CouponIssuance> issuanceBatch = new ArrayList<>();
-
         while (!queue.isEmpty()) {
-            IssuanceCouponRequest request = queue.poll();
-            if (request != null) {
-                Long amount = issuanceService.getCouponInventory(request.couponId());
+            var request = queue.poll();
+            Long amount = cacheService.getCouponInventory(request.couponId());
+            var issueMembers = cacheService.getIssueMembers(request.couponId());
+            if (amount > issueMembers.size()) {
+                cacheService.addIssueMember(request.userId(), request.couponId(), issueMembers);
+                cacheService.saveDuplicateCheck(request.userId(), request.couponId());
+            }
 
-                if (amount != null && amount > 0) {
-                    issuanceBatch.add(new CouponIssuance(request.couponId(), request.userId()));
-                    issuanceService.updateCouponInventory(request.couponId(), amount - 1);
-                    issuanceService.saveDuplicateCheck(request.userId(), request.couponId());
-                }
+            issueMembers = cacheService.getIssueMembers(request.couponId());
+            if (issueMembers.size() == amount) {
+                batchService.saveIssuancesToDatabase(request.couponId(), issueMembers);
+                cacheService.updateCouponInventory(request.couponId(), issueMembers.size() - amount);
             }
         }
-        issuanceService.saveIssuancesToDatabase(issuanceBatch);
     }
 }
