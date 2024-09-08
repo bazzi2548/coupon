aef1d4d23bc51e2a41603cc44af81f23b4956ee0
문정환 <bazzi2548@naver.com>
2024-09-08 17:33:35 +0900

    refactor: use CustomException

--- src/main/java/org/example/coupon/service/MemberService.java
+++ src/main/java/org/example/coupon/service/MemberService.java
@@ -5,10 +5,15 @@ import org.example.coupon.domain.Member;
 import org.example.coupon.dto.request.SignupMemberRequest;
 import org.example.coupon.dto.response.ResponseDTO;
 import org.example.coupon.dto.response.SignUpResponse;
+import org.example.coupon.exception.DuplicatedEmailException;
+import org.example.coupon.exception.DuplicatedNickNameException;
 import org.example.coupon.repository.MemberRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.example.coupon.exception.CustomExceptionMessage.DUPLICATE_EMAIL;
+import static org.example.coupon.exception.CustomExceptionMessage.DUPLICATE_NICKNAME;
 
 @Service
 @RequiredArgsConstructor
@@ -19,18 +24,27 @@ public class MemberService {
 
     @Transactional
     public ResponseDTO<SignUpResponse> signUp(SignupMemberRequest request) {
+
         validateDuplicatedEmail(request.email());
+        validateDuplicatedNickname(request.nickname());
+
         Member member = new Member(request, bCryptPasswordEncoder);
         memberRepository.save(member);
+
         return ResponseDTO.getSuccess(SignUpResponse.of(member));
     }
 
-    @Transactional(readOnly = true)
     public void validateDuplicatedEmail(String email) {
         boolean isDuplicated = memberRepository.existsByEmail(email);
         if (isDuplicated) {
-            throw new IllegalArgumentException("Email already in use");
-            // 추후 Custom Exception Class 구현 예정
+            throw new DuplicatedEmailException(DUPLICATE_EMAIL);
+        }
+    }
+
+    public void validateDuplicatedNickname(String nickname) {
+        boolean isDuplicated = memberRepository.existsByNickname(nickname);
+        if (isDuplicated) {
+            throw new DuplicatedNickNameException(DUPLICATE_NICKNAME);
         }
     }
 }
