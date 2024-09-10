3b11a88f4fae91d2344459e3f92c8c781ddcb98f
문정환 <70272622+bazzi2548@users.noreply.github.com>
2024-09-11 01:11:48 +0900

    feat: Redis 스케쥴러 위한 getCouponId Keys메소드 구현

--- src/main/java/org/example/coupon/service/CacheService.java
+++ src/main/java/org/example/coupon/service/CacheService.java
@@ -3,11 +3,14 @@ package org.example.coupon.service;
 import lombok.RequiredArgsConstructor;
 import org.example.coupon.domain.coupon.CouponInventory;
 import org.example.coupon.repository.coupon.CouponInventoryRepository;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -15,6 +18,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor
 public class CacheService {
     private final CouponInventoryRepository couponInventoryRepository;
+    private final CacheManager cacheManager;
 
     @Cacheable(value = "couponInventoryCache", key = "#couponId")
     @Transactional(readOnly = true)
@@ -48,5 +52,16 @@ public class CacheService {
     @CachePut(value = "duplicateCheckCache", key = "#memberId + ':' + #couponId")
     public boolean saveDuplicateCheck(Long memberId, Long couponId) {
         return true;
+    }
+
+    public List<Long> getCouponIdKeys() {
+        CaffeineCache couponInventoryCache = (CaffeineCache) cacheManager.getCache("couponInventoryCache");
+        return couponInventoryCache.getNativeCache()
+                .asMap()
+                .keySet()
+                .stream()
+                .mapToLong(i -> (long) i)
+                .boxed()
+                .toList();
     }
 }
