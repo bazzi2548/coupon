0bad30cc76963d49cada5659adf69cdcff13bb3e
문정환 <70272622+bazzi2548@users.noreply.github.com>
2024-09-13 02:15:53 +0900

    feat: 서비스 분리

--- src/main/java/org/example/coupon/service/CouponService.java
+++ src/main/java/org/example/coupon/service/CouponService.java
@@ -5,28 +5,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.coupon.dto.request.IssuanceCouponRequest;
 import org.example.coupon.exception.DuplicatedIssuanceException;
 import org.example.coupon.exception.ExhaustionException;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static org.example.coupon.exception.CustomExceptionMessage.DUPLICATE_ISSUANCE;
 import static org.example.coupon.exception.CustomExceptionMessage.EXHAUSTION;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class CouponService {
 
     private final CacheService cacheService;
-    private final RedisService redisService;
-    private final BatchService batchService;
 
-    private final ConcurrentLinkedQueue<IssuanceCouponRequest> queue = new ConcurrentLinkedQueue<>();
+    protected static final ConcurrentLinkedQueue<IssuanceCouponRequest> queue = new ConcurrentLinkedQueue<>();
 
     public String requestCouponIssuance(IssuanceCouponRequest request) {
 
@@ -41,36 +34,5 @@ public class CouponService {
         } else {
             throw new ExhaustionException(EXHAUSTION);
         }
-    }
-
-    @Async
-    @Scheduled(fixedRate = 1000)
-    public void processQueue() {
-        while (!queue.isEmpty()) {
-            var request = queue.poll();
-            Long amount = cacheService.getCouponInventory(request.couponId());
-            var issueMembers = cacheService.getIssueMembers(request.couponId());
-            if (amount > issueMembers.size()) {
-                cacheService.addIssueMember(request.memberId(), request.couponId(), issueMembers);
-                cacheService.saveDuplicateCheck(request.memberId(), request.couponId());
-            }
-        }
-    }
-
-    @Scheduled(fixedRate = 1000)
-    public void processBatch() {
-        List<Long> couponIdKeys = cacheService.getCouponIdKeys();
-        couponIdKeys.forEach(couponId -> {
-            long start = System.currentTimeMillis();
-            Long amount = cacheService.getCouponInventory(couponId);
-            Set<Long> issueMembers = cacheService.getIssueMembers(couponId);
-            Long size = redisService.luaScriptSetCoupon(couponId, amount, issueMembers);
-
-            if (Objects.equals(size, amount)){
-                cacheService.updateCouponInventory(couponId, -1L);
-            }
-
-            log.info("처리시간 = {} ms", System.currentTimeMillis() - start);
-        });
     }
 }
